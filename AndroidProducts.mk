93d7d9d97d0b8e506820cf44374c75ce5bedabc6
T.B.S <kuplemarkeple@gmail.com>
2022-11-03 03:42:19 +0700

    X00TD: lunch blaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/blaze_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug \
-    lineage_X00TD-user \
-    lineage_X00TD-eng
+    blaze_X00TD-userdebug \
+    blaze_X00TD-user \
+    blaze_X00TD-eng
