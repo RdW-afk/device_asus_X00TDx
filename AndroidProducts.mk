720deb0c77704e11413ec5c1cd17b6a4fa55009c
T.B.S <kuplemarkeple@gmail.com>
2022-11-03 21:12:54 +0700

    X00TD: corvus lunch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/corvus_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug \
-    lineage_X00TD-user \
-    lineage_X00TD-eng
+    corvus_X00TD-userdebug \
+    corvus_X00TD-user \
+    corvus_X00TD-eng
