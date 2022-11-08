b551f3cb4e5e66fa4fbc6aa6ddf1350c162884ac
T.B.S <kuplemarkeple@gmail.com>
2022-11-08 17:34:44 +0700

    X00TD: lunch octavi

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/octavi_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug \
-    lineage_X00TD-user \
-    lineage_X00TD-eng
+    octavi_X00TD-userdebug \
+    octavi_X00TD-user \
+    octavi_X00TD-eng
