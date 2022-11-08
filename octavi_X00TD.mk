f1225798b12c99e3ecf9a098fff792e483ed20ea
T.B.S <kuplemarkeple@gmail.com>
2022-11-08 17:36:16 +0700

    X00TD: Octavi bringup

--- octavi_X00TD.mk
+++ octavi_X00TD.mk
@@ -10,23 +10,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
-# Inherit some riceDroid stuff.
-RICE_MAINTAINER := dotkit
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-#TARGET_GAPPS_ARCH := arm64
-RICE_PACKAGE_TYPE := "AOSP"
-SUSHI_BOOTANIMATION := 720
-#TARGET_BUILD_GRAPHENEOS_CAMERA := true
-TARGET_ENABLE_BLUR := false
+# Bootanimation Res
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Octavi
+OCTAVI_BUILD_TYPE := Unofficial
+OCTAVI_MAINTAINER := dotkit
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := octavi_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
