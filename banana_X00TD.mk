c84f46ea8225cf7beb3890d48d943dcb8725ac30
T.B.S <kuplemarkeple@gmail.com>
2022-11-09 21:13:12 +0700

    X00TD: bananadroid bringup

--- banana_X00TD.mk
+++ banana_X00TD.mk
@@ -10,20 +10,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Bootanimation Res
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Octavi
-OCTAVI_BUILD_TYPE := Unofficial
-OCTAVI_MAINTAINER := dotkit
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+BANANA_BUILD_TYPE := UNOFFICIAL
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := octavi_X00TD
+PRODUCT_NAME := banana_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
